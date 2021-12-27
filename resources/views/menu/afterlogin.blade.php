b2e977e6555e2c90ad63a09cb5390d0099fcee73
lia <armaidalstr@gmail.com>
2021-12-27 16:29:01 +0700

    Update afterlogin.blade.php

--- resources/views/menu/afterlogin.blade.php
+++ resources/views/menu/afterlogin.blade.php
@@ -1,18 +1,20 @@
 @extends('layouts.main')
 @section('container')
 
-<div class="container">
-    <button>Quiz</button>
+<div class="d-grid gap-2 mx-auto">
+    <button type="button" class="btn btn-outline-danger">Quiz</button>
 </div>
-<div class="">
-    <button>Baca Materi</button>
+<div class="d-grid gap-2 mx-auto">
+    <button type="button" class="btn btn-outline-warning">Baca Materi</button>
 </div>
-<div class="">
-    <button>Leaderboard</button>
+<div class="d-grid gap-2 mx-auto">
+    <button type="button" class="btn btn-outline-success">Leaderboard</button>
 </div>
-<div class="">
-    <button>Profile</button>
+<div class="d-grid gap-2 mx-auto">
+    <button type="button" class="btn btn-outline-primary">Profile</button>
 </div>
+
+
 @endsection
 
 {{-- https://getbootstrap.com/docs/3.4/components/#navbar --}}

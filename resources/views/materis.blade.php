caaed03bd1e6c9d233bae9aaf63559920db94f86
lia <armaidalstr@gmail.com>
2022-01-01 13:28:40 +0700

    new

--- resources/views/materis.blade.php
+++ resources/views/materis.blade.php
@@ -5,7 +5,7 @@
 <h1> Materi Sejarah Kelas 11 </h1>
 
 @foreach ($sej11_level as $level)
-<a href="/kumpulan-materi/{{ $level->id }}"><button type="button" class="btn btn-secondary">{{ $level->judul_sub_bab }}</button></a>
+<li><a href="/kumpulan-materi/{{ $level->id }}"><button type="button" class="btn btn-outline-secondary">{{ $level->judul_sub_bab }}</button></a></li><br>
 @endforeach
 @endsection
 
--- resources/views/menu/afterlogin.blade.php
+++ resources/views/menu/afterlogin.blade.php
@@ -6,13 +6,15 @@
     <button type="button" class="btn btn-outline-danger">Quiz</button>
 </div>
 <div class="d-grid gap-2 mb-3 mx-auto">
-    <button type="button" class="btn btn-outline-warning">Baca Materi</button>
+    {{-- <button type="button" ></button> --}}
+    <a href="/kumpulan-materi" class="btn btn-outline-warning">Baca Materi</a>
 </div>
 <div class="d-grid gap-2 mb-3 mx-auto">
     <button type="button" class="btn btn-outline-success">Leaderboard</button>
 </div>
 <div class="d-grid gap-2 mb-3 mx-auto">
-    <button type="button" class="btn btn-outline-primary">Profile</button>
+    {{-- <button type="button" class="btn btn-outline-primary">Profile</button> --}}
+    <a href="/profile" class="btn btn-outline-primary">Profile</a>
 </div>
 
 

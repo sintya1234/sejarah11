14702b2ccf701dfe5d0565bc6fa8a6bc451d8ae7
davidalex-dev <84223537+davidalex-dev@users.noreply.github.com>
2022-01-03 14:05:56 +0700

    Nambah tombol pergi ke materi sblmnya/brikutnya
    Di halaman materi

--- resources/views/materi.blade.php
+++ resources/views/materi.blade.php
@@ -5,6 +5,12 @@
 <div class="container">
     <div class="row justify-content-center mb-5">
         <div class="col-mb-3">
+            <a href="/kumpulan-materi" class="btn btn-secondary">Kembali ke List Materi</a>
+
+            <a href="/kumpulan-materi/{{ $sej11_level -> id - 1}}" class="btn btn-secondary">Pergi ke materi sebelumnya</a>
+
+            <a href="/kumpulan-materi/{{ $sej11_level -> id + 1}}" class="btn btn-secondary">Pergi ke materi berikutnya</a>
+
             <h1 class="mb-3">{{ $sej11_level->judul_sub_bab }}</h1>
 
             <img src="{{ $sej11_level->gambar_utuh }}"
@@ -13,7 +19,6 @@
             <article class="my-3 fs-5">
                 {!! $sej11_level->materi !!}{{-- supaya yag html terpakai tapi tidak dicetak good --}}
             </article>
-            <a href="/kumpulan-materi" class="d-block mt-3">Kembali ke List materi</a>
         </div>
     </div>
 </div>
